34d679a040dbcaf1ef881596d368c26ab0bd7561
Alina <almaksimovic01@gmail.com>
2025-04-01 23:13:52 +0400

    Finish hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const hash = createHash("sha256");
+  const stream = createReadStream(filePath);
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    const hexHash = hash.digest("hex");
+    console.log(hexHash);
+  });
+
+  stream.on("error", (err) => {
+    console.error(err.message);
+  });
 };
 
 await calculateHash();
