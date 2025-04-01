b28ebb8439ee6d6e0591058340d0850fb5cea36b
Alina <almaksimovic01@gmail.com>
2025-04-01 22:05:54 +0400

    Finish cli subtask

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVars = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  if (rssVars) {
+    console.log(rssVars);
+  } else {
+    console.log("No RSS_ environment variables found");
+  }
 };
 
 parseEnv();
